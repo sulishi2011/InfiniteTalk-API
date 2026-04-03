91751372718c5c59f1d9812837f899d64a07d22b
sulishi2011 <1121253715@qq.com>
2026-04-03 16:38:48 +0800

    Disable wav2vec attentions for sdpa compatibility

--- src/audio_analysis/wav2vec2.py
+++ src/audio_analysis/wav2vec2.py
@@ -20,7 +20,7 @@ class Wav2Vec2Model(Wav2Vec2Model):
         output_hidden_states=None,
         return_dict=None,
     ):
-        self.config.output_attentions = True
+        output_attentions = output_attentions if output_attentions is not None else False
 
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -84,7 +84,7 @@ class Wav2Vec2Model(Wav2Vec2Model):
         output_hidden_states=None,
         return_dict=None,
     ):
-        self.config.output_attentions = True
+        output_attentions = output_attentions if output_attentions is not None else False
 
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
